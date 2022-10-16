d902eb52fef1d3f2365010bdc8a6eae0ea566487
Ilya195 <vodoley_agent@inbox.ru>
2022-10-16 22:22:57 +0600

    Changed task file

--- task.js
+++ task.js
@@ -1,16 +1,13 @@
+
 function compareArrays(arr1, arr2) {
-  let result = false;
-  if (arr1.length == arr2.length) {
-    result = arr1.every((element, index) => {
-      return element === arr2[index]} );
-  }
-  return result; 
+  return arr1.length === arr2.length &&
+  arr1.every((item, idx) => item === arr2[idx]); 
 }
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((element,int,arr) => {
-    if (element > 0 && element % 3 === 0) {
-      return element }
-    }).map(item => item*10);
-  return resultArr; 
+  return arr.filter(element => element > 0 && element % 3 === 0)
+  .map(element => element * 10);
 }
+  
+
+  
